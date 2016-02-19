a5359779ae51ca4bfdb2b261f14d64d7c01690d6
Jeff Tratner <jtratner@counsyl.com>
2016-02-19 14:14:21 -0800

    Switch python package name to swift_commander

--- setup.py
+++ setup.py
@@ -6,20 +6,20 @@ setup(
     description='''\
 swift commander (swc) is a wrapper to various command line
 client tools for openstack swift cloud storage systems.''',
-    packages=['swc'],
-    scripts=['swc/swc'],
+    packages=['swift_commander'],
+    scripts=['swift_commander/swc'],
     entry_points={
         # we use console_scripts here to allow virtualenv to rewrite shebangs
         # to point to appropriate python and allow experimental python 2.X
         # support.
         'console_scripts': [
-            'swbundler.py=swc.swbundler:main',
-            'swfoldersize.py=swc.swfoldersize:main',
-            'swhashcomp.py=swc.swhashcomp:main',
-            'swpget.py=swc.swpget:main',
-            'swrm.py=swc.swrm:main',
-            'swsearch.py=swc.swsearch:main',
-            'swsymlinks.py=swc.swsymlinks:main',
+            'swbundler.py=swift_commander.swbundler:main',
+            'swfoldersize.py=swift_commander.swfoldersize:main',
+            'swhashcomp.py=swift_commander.swhashcomp:main',
+            'swpget.py=swift_commander.swpget:main',
+            'swrm.py=swift_commander.swrm:main',
+            'swsearch.py=swift_commander.swsearch:main',
+            'swsymlinks.py=swift_commander.swsymlinks:main',
         ]
     }
 )
