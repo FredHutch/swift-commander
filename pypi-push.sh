6e4cb3ee37546d3b0f7c40d2f9bbc9c00240a4e8
dirkpetersen <petersen@fhcrc.org>
2016-03-12 10:33:19 -0800

    version 1.3.7

--- pypi-push.sh
+++ pypi-push.sh
@@ -5,6 +5,6 @@ version=$(grep ^__version__ setup.py | cut -d'"' -f2)
 git commit -a -m "version ${version}"
 git tag ${version} -m "tag for PyPI"
 git push --tags origin master
-python3 setup.py register -r pypitest
-python3 setup.py sdist upload -r pypitest
+python3 setup.py register -r pypi
+python3 setup.py sdist upload -r pypi
 
