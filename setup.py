50a5790fd012b8eda06b3ee02eb6d4e1d1cb9a94
phsdemo3 <petersen@fhcrc.org>
2016-03-11 19:41:12 -0800

    more pypi attributes

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
 from setuptools import setup
 
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Cloud Storage :: Libraries :: Python Modules",
+]
+
 setup(
     name='swift-commander',
     version='1.3',
@@ -13,7 +32,8 @@ client tools for openstack swift cloud storage systems.''',
     url = 'https://github.com/FredHutch/swift-commander', 
     download_url = 'https://github.com/FredHutch/swift-commander/tarball/1.3',
     keywords = ['openstack', 'swift', 'cloud storage'], # arbitrary keywords
-    classifiers = [],
+    classifiers = CLASSIFIERS,
+    install_requires=['python-swiftclient>=2.5,<3', 'python-keystoneclient>=1.5,<2']
     entry_points={
         # we use console_scripts here to allow virtualenv to rewrite shebangs
         # to point to appropriate python and allow experimental python 2.X
