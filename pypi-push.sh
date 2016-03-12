54fb1e83dc61e2edfb11d9b4626301d2f4ff52fa
dirkpetersen <petersen@fhcrc.org>
2016-03-12 12:06:50 -0800

    version 1.3.8

--- pypi-push.sh
+++ pypi-push.sh
@@ -8,3 +8,9 @@ git push --tags origin master
 python3 setup.py register -r pypi
 python3 setup.py sdist upload -r pypi
 
+echo "  Done! Occasionally you may want to remove older tags:"
+echo "git tag 1.2.3 -d"
+echo "git push origin :refs/tags/1.2.3"
+
+
+
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = "1.3.7"
+__version__ = "1.3.8"
 
 try:
     from pypandoc import convert
